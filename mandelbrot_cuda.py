d6a91a7deab884284b4e2e22f04f87aac97db219
James W. McClain <jmcclain@daystrom-data-concepts.com>
2026-02-27 22:51:38 -0500

    Update Python host code

--- mandelbrot_cuda.py
+++ mandelbrot_cuda.py
@@ -2,13 +2,21 @@ import numpy as np
 import pycuda.autoinit
 import pycuda.driver as cuda
 
+from mandelbrot_precision import choose_precision
+
 
 def load_kernel(compiled_file="mandelbrot.ptx"):
     mod = cuda.module_from_file(compiled_file)
-    return mod.get_function("mandelbrot")
+    return {
+        "float32": mod.get_function("mandelbrot_f32"),
+        "float64": mod.get_function("mandelbrot_f64"),
+    }
 
 
-def run_kernel(kernel, WIDTH, HEIGHT, max_iter, x_min, x_max, y_min, y_max):
+def run_kernel(kernels, WIDTH, HEIGHT, max_iter, x_min, x_max, y_min, y_max, precision="auto"):
+    if precision == "auto":
+        precision = choose_precision(WIDTH, HEIGHT, max_iter, x_min, x_max, y_min, y_max)
+
     output_host = np.zeros((HEIGHT, WIDTH), dtype=np.int32)
     output_gpu = cuda.mem_alloc(output_host.nbytes)
 
@@ -16,14 +24,17 @@ def run_kernel(kernel, WIDTH, HEIGHT, max_iter, x_min, x_max, y_min, y_max):
     blocks_x = (WIDTH + threads_per_block[0] - 1) // threads_per_block[0]
     blocks_y = (HEIGHT + threads_per_block[1] - 1) // threads_per_block[1]
 
+    kernel = kernels[precision]
+    real_dtype = np.float32 if precision == "float32" else np.float64
+
     kernel(output_gpu,
            np.int32(WIDTH),
            np.int32(HEIGHT),
            np.int32(max_iter),
-           np.float32(x_min),
-           np.float32(x_max),
-           np.float32(y_min),
-           np.float32(y_max),
+           real_dtype(x_min),
+           real_dtype(x_max),
+           real_dtype(y_min),
+           real_dtype(y_max),
            block=threads_per_block,
            grid=(blocks_x, blocks_y, 1))
 
